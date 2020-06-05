dbe982fa5d6bf423cdee64e19aefe48c8a39e947
Hollie Kestler <hollie.kestler@g.austincc.edu>
2020-06-04 21:02:47 -0500

    Assignment 1

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -6,8 +6,18 @@ int sum_numbers(int num1, int num2)
 {
 	return num1 + num2;
 }
+/*
+Function multiples num1 with local variable num2
 
+@param int num1
+*/
+int multiply_numbers(int num1)
+{
+	auto num2 = 5;
+	auto result = num1 * num2;
 
+	return result;
+}
 
 
 
