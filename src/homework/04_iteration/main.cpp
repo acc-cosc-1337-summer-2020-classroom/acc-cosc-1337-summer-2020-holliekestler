503a403825237d5af614d079a581bfb8d61134c7
Hollie Kestler <hollie.kestler@g.austincc.edu>
2020-06-27 00:51:05 -0500

    Homework 3

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -15,27 +15,28 @@ int main()
 {
 	int choice;
 	string dna;
-	do
-	{
-		cout<<"Enter 1 to get GC content or 2 to get DNA complement. Enter 'q' to quit. \n";
+	while (true) {
+		cout<<"Enter 1 to get GC content or 2 to get DNA complement. Enter 'y' or 'Y' to quit. \n";
 		cin>>choice;
-		cout<<"Enter the DNA string: \n";
-		cin>>dna;
 
 		if(choice == 1)
 		{
+			cout<<"Enter the DNA string: \n";
+			cin>>dna;
 			cout<<get_gc_content(dna);			
 		}
 		else if(choice == 2)
 		{
+			cout<<"Enter the DNA string: \n";
+			cin>>dna;
 			cout<<get_dna_complement(dna);			
 		}
 		else
 		{
-			cout<<"Invalid response.";
+			break;
 		}
 		
-	} while (choice != 'q');
+	}
 	
 	return 0;
 }
