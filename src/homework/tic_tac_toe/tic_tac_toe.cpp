5039310a4151d5f31cc197b97a33d164f00ab33a
Hollie Kestler <hollie.kestler@g.austincc.edu>
2020-07-10 23:49:25 -0500

    Tic Tac Toe

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,117 +1,108 @@
 //cpp
 #include "tic_tac_toe.h"
 
-bool TicTacToe::game_over()
-{
-    if(check_row_win() || check_column_win() || check_diagonal_win())
-    {
+bool TicTacToe::game_over() {
+    if (check_row_win() || check_column_win() || check_diagonal_win()) {
         set_winner();
         return true;
-    } else if(check_board_full())
-    {
+    } else if (check_board_full()) {
         winner = "C";
         return true;
-    } else
-    {
+    } else {
         return false;
     }
 }
 
-void TicTacToe::start_game(std::string first_player)
-{
+void TicTacToe::start_game(std::string first_player) {
     player = first_player;
     clear_board();
 }
 
-void TicTacToe::mark_board(int position)
-{
-    pegs[position-1] = player;
+void TicTacToe::mark_board(int position) {
+    pegs[position - 1] = player;
     set_next_player();
 }
 
-std::string TicTacToe::get_player() const {return player;}
+std::string TicTacToe::get_player() const { return player; }
 
-void TicTacToe::display_board()const
-{
-    for(std::size_t i=0; i < pegs.size(); i += 3)
-    {
-        std::cout<<pegs[i]<<"|"<<pegs[i+1]<<"|"<<pegs[i+2]<<"\n";
+void TicTacToe::display_board() const {
+    for (std::size_t i = 0; i < pegs.size(); i += 3) {
+        std::cout << pegs[i] << "|" << pegs[i + 1] << "|" << pegs[i + 2] << "\n";
     }
 }
 
-bool TicTacToe::check_column_win()
-{
-    if(pegs[0] != " " && pegs[3] != " " && pegs[6] != " ")
-    {
-        return pegs[0] == pegs[3] && pegs[3] == pegs[6];
+bool TicTacToe::check_column_win() {
+    if (pegs[0] != " " && pegs[3] != " " && pegs[6] != " ") {
+        if (pegs[0] == pegs[3] && pegs[3] == pegs[6]) {
+            return true;
+        }
     }
-    if(pegs[1] != " " && pegs[4] != " " && pegs[7] != " ")
-    {
-        return pegs[1] == pegs[4] && pegs[4] == pegs[7];
+    if (pegs[1] != " " && pegs[4] != " " && pegs[7] != " ") {
+        if (pegs[1] == pegs[4] && pegs[4] == pegs[7]) {
+            return true;
+        }
     }
-    if(pegs[2] != " " && pegs[5] != " " && pegs[8] != " ")
-    {
-        return pegs[2] == pegs[5] && pegs[5] == pegs[8];
-    } return false;
+    if (pegs[2] != " " && pegs[5] != " " && pegs[8] != " ") {
+        if (pegs[2] == pegs[5] && pegs[5] == pegs[8]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-bool TicTacToe::check_row_win()
-{
-    if(pegs[0] != " " && pegs[1] != " " && pegs[2] != " ")
-    {
-        return pegs[0] == pegs[1] && pegs[1] == pegs[2];
+bool TicTacToe::check_row_win() {
+    if (pegs[0] != " " && pegs[1] != " " && pegs[2] != " ") {
+        if (pegs[0] == pegs[1] && pegs[1] == pegs[2]) {
+            return true;
+        }
     }
-    if(pegs[3] != " " && pegs[4] != " " && pegs[5] != " ")
-    {
-        return pegs[3] == pegs[4] && pegs[4] == pegs[5];
+    if (pegs[3] != " " && pegs[4] != " " && pegs[5] != " ") {
+        if (pegs[3] == pegs[4] && pegs[4] == pegs[5]) {
+            return true;
+        }
     }
-    if(pegs[6] != " " && pegs[7] != " " && pegs[8] != " ")
-    {
-        return pegs[6] == pegs[7] && pegs[7] == pegs[8];
-    } return false;
+    if (pegs[6] != " " && pegs[7] != " " && pegs[8] != " ") {
+        if (pegs[6] == pegs[7] && pegs[7] == pegs[8]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-bool TicTacToe::check_diagonal_win()
-{
-    if(pegs[0] != " " && pegs[4] != " " && pegs[8] != " ")
-    {
-        return pegs[0] == pegs[4] && pegs[4] == pegs[8];
+bool TicTacToe::check_diagonal_win() {
+    if (pegs[0] != " " && pegs[4] != " " && pegs[8] != " ") {
+        if (pegs[0] == pegs[4] && pegs[4] == pegs[8]) {
+            return true;
+        }
     }
-    if(pegs[6] != " " && pegs[4] != " " && pegs[2] != " ")
-    {
-        return pegs[6] == pegs[4] && pegs[4] == pegs[2];
-    } return false;
+    if (pegs[6] != " " && pegs[4] != " " && pegs[2] != " ") {
+        if (pegs[6] == pegs[4] && pegs[4] == pegs[2]) {
+            return true;
+        }
+    }
+    return false;
 }
 
-void TicTacToe::set_winner()
-{
-    if(player == "X")
-    {
+void TicTacToe::set_winner() {
+    if (player == "X") {
         winner = "O";
     } else {
         winner = "X";
     }
 }
 
-void TicTacToe::set_next_player()
-{
-    if(player == "X")
-    {
+void TicTacToe::set_next_player() {
+    if (player == "X") {
         player = "O";
-    }
-    else {
+    } else {
         player = "X";
     }
 }
 
 
-
-bool TicTacToe::check_board_full()const
-{
-    for(auto peg: pegs)
-    {
-        if(peg == " ")
-        {
+bool TicTacToe::check_board_full() const {
+    for (auto peg: pegs) {
+        if (peg == " ") {
             return false;
         }
     }
@@ -119,10 +110,8 @@ bool TicTacToe::check_board_full()const
     return true;
 }
 
-void TicTacToe::clear_board()
-{
-    for(std::size_t i=0; i < pegs.size(); ++i)
-    {
+void TicTacToe::clear_board() {
+    for (std::size_t i = 0; i < pegs.size(); ++i) {
         pegs[i] = " ";
     }
 }
