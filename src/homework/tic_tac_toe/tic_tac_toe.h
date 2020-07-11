9edbd6c89a52e40c6491eaff630e62639c4151ff
Hollie Kestler <hollie.kestler@g.austincc.edu>
2020-07-10 23:49:56 -0500

    Tic Tac Toe

--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -6,20 +6,38 @@
 #ifndef TIC_TAC_TOE_H
 #define TIC_TAC_TOE_H
 
-class TicTacToe
-{
+class TicTacToe {
 public:
     bool game_over();
+
     void mark_board(int position);
+
     void start_game(std::string first_player);
-    void display_board()const;
-    std::string get_player()const{return player;}
+
+    void display_board() const;
+
+    std::string get_player() const;
+
+    std::string get_winner();
+
 private:
-    void set_next_player();
-    bool check_board_full()const;
-    void clear_board();
     std::string player;
     std::vector<std::string> pegs{9, " "};
+    std::string winner;
+
+    void set_next_player();
+
+    bool check_board_full() const;
+
+    void clear_board();
+
+    bool check_column_win();
+
+    bool check_row_win();
+
+    bool check_diagonal_win();
+
+    void set_winner();
 };
 
 #endif
