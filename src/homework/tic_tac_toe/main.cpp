d146a06b4e7f408c148b3414dbb6b2138024da69
Hollie Kestler <hollie.kestler@g.austincc.edu>
2020-07-10 23:49:48 -0500

    Tic Tac Toe

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,26 +1,33 @@
 #include "tic_tac_toe.h"
 
-int main() 
-{
-	TicTacToe tic_tac_toe;
-	std::string player;
+int main() {
+    TicTacToe tic_tac_toe;
+    std::string player;
+    std::string choice;
 
-	std::cout<<"Enter X or O: ";
-	std::cin>>player;
+    do {
+        std::cout << "Enter X or O: ";
+        std::cin >> player;
 
-	tic_tac_toe.start_game(player);
-	
-	do
-	{
-		int position;
-		std::cout<<"Enter position from 1 to 9: ";
-		std::cin>>position;
-		tic_tac_toe.mark_board(position);
-		tic_tac_toe.display_board();
+        if (player != "X" && player != "O") {
+            std::cout << "You can only choose X or O." << '\n';
+        }
+    } while (player != "X" && player != "O");
 
-	} while (tic_tac_toe.game_over() == false);
-	
-	std::cout<<"Game over.";
+    tic_tac_toe.start_game(player);
 
-	return 0;
+    do {
+        int position;
+        std::cout << "Enter position from 1 to 9: ";
+        std::cin >> position;
+        tic_tac_toe.mark_board(position);
+        tic_tac_toe.display_board();
+
+    } while (!tic_tac_toe.game_over());
+
+    std::cout << "Game over. ";
+    std::cout << "The winner is: " << tic_tac_toe.get_winner() << "!" << '\n';
+
+    std::cout << "Would you like to play again? If so, enter y or Y: ";
+    std::cin >> choice;
 }
